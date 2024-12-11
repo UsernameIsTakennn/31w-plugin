1be40e11cf94bc4377100c61b6611a2170b2fd21
Unnom <benserker70@gmail.com>
2024-12-11 10:38:32 -0500

    s5c10 Enfiler les chemins pour style.css et carrousel.js

--- carrousel.php
+++ carrousel.php
@@ -12,6 +12,25 @@
             - créer une feuille de style pour la boite modale
             - Script permettant d'ouvrir et fermer la boite modale
     */
+function enfile_css_script(){
+    $version_css = filemtime(plugin_dir_path(__FILE__). "/style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__). "/js/carrousel.js");
+
+    wp_enqueue_style(
+        "carrousel",
+        array(),
+        plugin_dir_url(__FILE__). "/style.css"
+    );
+    wp_enqueue_script(
+        "carrousel",
+        array(),
+        plugin_dir_url(__FILE__) . "/js/carrousel.js",
+        true
+    );
+}
+
+add_action("wp_enqueue_script", "enfile_css_script");
+
 function genere_carrousel()
 {
     $chaine = '<button id="carrousel__bouton" class="carrousel__bouton">Ouvrir</button>

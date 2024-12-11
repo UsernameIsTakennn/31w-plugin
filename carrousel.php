12883f988146a03091014795eb06f95df9b6ede8
Unnom <benserker70@gmail.com>
2024-12-11 10:19:52 -0500

    s5c10 fonction de la carrousel

--- carrousel.php
+++ carrousel.php
@@ -1,13 +1,28 @@
 <?php
-    /*
+/*
     plugin name: Carrousel
     description: Carrousel permettant d'afficher le contenu d'une galerie
     author: Splenderly Jean Louis
     author url:https://referenced.ca
     */
-    function ecrire_message(){
-        return "Bonjour tout le monde";
-    }
 
-    add_shortcode("message", "ecrire_message");
+/*
+            - génère le code HTML d'une partie de la boite modale
+            - créer un bouton qui permettra d'ouvrir la boite modale
+            - créer une feuille de style pour la boite modale
+            - Script permettant d'ouvrir et fermer la boite modale
+    */
+function genere_carrousel()
+{
+    $chaine = '<button id="carrousel__bouton" class="carrousel__bouton">Ouvrir</button>
+                <div class="carrousel">
+                    <button class="carrousel__X">X</button>
+                    <figure class="carrousel__figure"></figure>
+                    <form class="carrousel__form"></form>
+                </div>';
+
+    return $chaine;
+}
+
+add_shortcode("carrousel", "genere_carrousel");
 ?>

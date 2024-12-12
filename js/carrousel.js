58fc3f4f472c77fb075490a5eb7b090579e8783a
Unnom <benserker70@gmail.com>
2024-12-11 21:31:52 -0500

    s5c10 Fonctionnalité du bouton de la carrousel javascript

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,14 @@
 (function(){
     console.log("Vive Javascript");
-    
+    let carrousel = document.querySelector('.carrousel')
+    let carrousel__bouton = document.querySelector('.carrousel__bouton')
+    let carrousel__X = document.querySelector('.carrousel__X')
+
+    carrousel__bouton.addEventListener('mousedown', function(){
+        carrousel.classList.add('carrousel--ouvrir')
+    })
+
+    carrousel__X.addEventListener("mousedown", function () {
+        carrousel.classList.remove("carrousel--ouvrir");
+    });
 })()

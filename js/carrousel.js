d1417e7f5b51ad16be0342e18b1b51cc0f295012
Unnom <benserker70@gmail.com>
2024-12-12 01:19:14 -0500

    s5c10 Changement mineur

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,14 +1,15 @@
-(function(){
-    console.log("Vive Javascript");
-    let carrousel = document.querySelector('.carrousel')
-    let carrousel__bouton = document.querySelector('.carrousel__bouton')
-    let carrousel__X = document.querySelector('.carrousel__X')
+(function () {
+  console.log("Vive Javascript");
 
-    carrousel__bouton.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--ouvrir')
-    })
+  let carrousel = document.querySelector(".carrousel");
+  let carrousel__bouton = document.querySelector(".carrousel__bouton");
+  let carrousel__X = document.querySelector(".carrousel__X");
 
-    carrousel__X.addEventListener("mousedown", function () {
-        carrousel.classList.remove("carrousel--ouvrir");
-    });
-})()
+  carrousel__bouton.addEventListener("mousedown", function () {
+    carrousel.classList.add("carrousel--ouvrir");
+  });
+
+  carrousel__X.addEventListener("mousedown", function () {
+    carrousel.classList.remove("carrousel--ouvrir");
+  });
+})();
